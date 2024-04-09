7aff9cdb86aeca7c6b72657c26140a019dc78239
Ryan MI <ryanmuhammad2004irfanm@gmail.com>
2024-04-09 22:13:06 +0700

    commit

--- PRAKTIKUM1/PRAK103.php
+++ PRAKTIKUM1/PRAK103.php
@@ -2,11 +2,11 @@
 
 $celcius = 37.841;
 
-$fahrenhait = 9/5 * $celcius + 32;
+$fahrenheit = 9/5 * $celcius + 32;
 $reamur = 4/5 * $celcius;
 $kelvin = $celcius + 273;
 
-echo "Fahrenhait (F) = ", number_format($fahrenheit, 4, ','), "<br>";
+echo "Fahrenheit (F) = ", number_format($fahrenheit, 4, ','), "<br>";
 echo "Reamur (R) = " , number_format($reamur, 4, ','), "<br>";
 echo "Kelvin (K) = " , number_format($kelvin, 4, ','), "<br>";1
 
